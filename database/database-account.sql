bc724434f0df8a1bd78eb6d6ce7398f189cf449a
Vaishakh Vibudhendran Nair <vibudhvh@mail.uc.edu>
2024-07-31 18:48:43 +0800

    sprint 0

---
+++ database/database-account.sql
@@ -0,0 +1,3 @@
+create database waph_team;
+CREATE USER 'vibudhvh'@'localhost' IDENTIFIED BY '1010';
+GRANT ALL ON waph_team.* TO 'viduvhvh'@'localhost';
---
+++ database/database-data.sql
@@ -0,0 +1,2 @@
+insert into users(username, password) Values ('vibudhvh', md5('1010'));
+
